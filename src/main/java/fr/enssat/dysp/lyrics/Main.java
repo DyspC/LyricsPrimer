33f5d1b2fd9759ee02768cc879c3ca9df68bcef7
Dysp <c.desc2@gmail.com>
2021-10-09 20:55:40 +0200

    Try fatjar build

---
+++ src/main/java/fr/enssat/dysp/lyrics/Main.java
@@ -0,0 +1,10 @@
+package fr.enssat.dysp.lyrics;
+
+import fr.enssat.dysp.lyrics.ui.LyricsPrimerFx;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LyricsPrimerFx.main(args);
+    }
+}
